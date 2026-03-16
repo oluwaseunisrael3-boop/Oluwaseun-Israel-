16b9d4c83cab93b85da1e8c60ad61146f9e9cccf
oluwaseunisrael3-boop <oluwaseunisrael3@gmail.com>
2026-03-16 04:21:14 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+const devotional = document.querySelector(".devotional-content");
+const verse = document.querySelector(".key-verse");
+
+if(devotional){
+devotional.innerHTML = "Welcome to the GOZOM International Daily Devotional.";
+}
+
+if(verse){
+verse.innerHTML = "Psalm 119:105 - Thy word is a lamp unto my feet.";
+}
+
+});
